edbcad80f46435515ea9f7678bf8ac79579f6c99
Igor Drobiazko <igor@elastic.io>
2016-11-07 20:22:34 +0100

    More logs

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -4,10 +4,14 @@ const client = require('./lib/client.js');
 module.exports = verify;
 
 function verify(credentials) {
+    console.log('About to verify given API key by retrieving user from Mandrill');
 
     const client = client.create(credentials);
 
     return new Promise((resolve, reject) => {
         return client.users.info({}, resolve, reject);
+    }).then((result) => {
+        console.log('Your username: %s. API key is valid.', result.username);
+        return result;
     });
 }
